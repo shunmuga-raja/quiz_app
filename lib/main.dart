e33c9b95b47f1c928d1a8a8b0ca4d43187432938
shunmuga-raja <84874487+shunmuga-raja@users.noreply.github.com>
2023-05-22 07:23:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data_uploader_screen.dart';
+import 'package:quiz_app/firebase_options.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    GetMaterialApp(
+      home: DataUploaderScreen(),
+    ),
+  );
+}
