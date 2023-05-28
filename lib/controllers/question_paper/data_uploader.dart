559ef6f47cd446955c4656aa16c63d785ccea2bf
shunmuga-raja <84874487+shunmuga-raja@users.noreply.github.com>
2023-05-28 18:52:01 +0530

    generate database

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:quiz_app/modles/question_paper_model.dart';
 
 //controller
 class DataUploader extends GetxController {
@@ -23,9 +24,13 @@ class DataUploader extends GetxController {
         .where((path) =>
             path.startsWith("assets/DB/paper") && path.contains(".json"))
         .toList();
-        for(var paper in papersInAssets){
-          String stringPaperContent = await rootBundle.loadString(paper);
-          print(stringPaperContent);
-        }
+
+    List<QuestionPaperModel> questionPapers = [];
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      questionPapers
+          .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+    }
+    print('Items number ${questionPapers.length}');
   }
 }
